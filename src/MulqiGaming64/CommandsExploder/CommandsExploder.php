3c1d22b12c5c053db8fcf9c576f8840ecb5c9fe5
MulqiGaming64 <mulqi311@gmail.com>
2021-09-10 05:22:06 +0700

    Create CommandsExploder.php

---
+++ src/MulqiGaming64/CommandsExploder/CommandsExploder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MulqiGaming64\CommandsExploder;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\event\server\ServerCommandEvent;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class CommandsExploder extends PluginBase implements Listener {
+	
+    public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onExecuteCommand(ServerCommandEvent $ev){
+		$command = $ev->getCommand();
+		$exp = explode(" && ", $command);
+		$sender = $ev->getSender();
+		if($sender instanceof Player){
+			$ev->setCommand($exp[0]);
+			unset($exp[0]);
+			foreach($exp as $cmd){
+				$this->getServer()->getCommandMap()->dispatch($sender, $cmd);
+			}
+			return true;
+		} else {
+			$ev->setCommand($exp[0]);
+			unset($exp[0]);
+			foreach($exp as $cmd){
+				$this->getServer()->getCommandMap()->dispatch(new ConsoleCommandSender(), $cmd);
+			}
+			return true;
+		}
+	}
+}
